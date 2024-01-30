48be0d2224fdb6bd2c1b380eb06557c8f5408a29
Basem Saleh <mreagles524@gmail.com>
2024-01-29 22:17:20 -0500

    started portfolio website

---
+++ src/portfolio.js
@@ -0,0 +1,70 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://mreagles524.github.io',
+  title: 'BS.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Basem Saleh',
+  role: 'Machine Learning Engineer',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com/in/basem-a-saleh',
+    github: 'https://github.com/mreagles524',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'Python',
+  'PyTorch',
+  'TensorFlow',
+  'Scikit Learn',
+  'AWS S3',
+  'Docker',
+  'KubeFlow',
+  'RoboFlow',
+  'Software Defined Radios',
+  'Git',
+  'CI/CD',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'johnsmith@mail.com',
+}
+
+export { header, about, projects, skills, contact }
